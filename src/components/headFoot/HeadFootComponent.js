7f7c9cea132b416d2f520cbc25752317b6e543d7
Scott Alan <scottalandev@gmail.com>
2021-12-08 22:39:08 -0800

    footer, responsive layout

--- src/components/headFoot/HeadFootComponent.js
+++ src/components/headFoot/HeadFootComponent.js
@@ -13,7 +13,7 @@ export function Header() {
 						<img src={wpsLogo} alt="Willamette Piano Service logo" className="wps-logo" />
 					</NavLink>
 				</div>
-				<nav className="flex-row nav-links">
+				<nav className="flex-row head-nav">
 					<NavLink to="/services">Services</NavLink>
 					<NavLink to="/contact">Contact</NavLink>
 				</nav>
@@ -25,9 +25,27 @@ export function Header() {
 export function Footer(props) {
 	let year = new Date().getFullYear();
 	return (
-		<div className="container-fluid bg-dark text-light footer-content">
-			<div className="row">
-				<p className="grey-text"> &copy; {year} Willamette Piano Service</p>
+		<div className="container-fluid footer-container">
+			<div className="row align-items-center">
+				<div className="d-none d-md-inline col-md-4">
+					<img src={wpsLogo} style={{ height: '100px' }} alt="Willamette Piano Service logo" />
+				</div>
+                <div className="col col-md-8">
+                    <div className="row align-items-center">
+                        <div className="col-sm-3 offset-sm-2 offset-md-0">
+                            <nav className="nav foot-nav flex-column text-center text-sm-start">
+                                <NavLink to="/">Home</NavLink>
+                                <NavLink to="/services">Services</NavLink>
+                                <NavLink to="/contact">Contact</NavLink>
+                            </nav>
+                        </div>
+                        <div className="col-sm-7 foot-text">
+                            <a href="mailto:willamettepiano@gmail.com">willamettepiano@gmail.com</a>
+                            <hr />
+                            <p> &copy; {year} Willamette Piano Service</p>
+                        </div>
+                    </div>
+                </div>
 			</div>
 		</div>
 	);
